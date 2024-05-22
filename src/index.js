531b486430b152f91b22e444c5921f263315062e
reeganv13 <81911939+reeganv13@users.noreply.github.com>
2024-05-22 21:45:11 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -10,7 +11,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename='/Heliverse-React-Project'>
+      <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
